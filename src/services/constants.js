b999ddc26cc52759da1177606f5dc26a17be94f0
Amari Princivil <ajthacode88@gmail.com>
2022-04-04 19:51:15 -0400

    add home

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,15 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
-const CLIENT_URL = "https://api.tvmaze.com/search/shows?q="
+const CLIENT_URL = "https://api.tvmaze.com/search/shows?q=";
 
-export async function getTvShowData(input){
-    try {
-        //making api call
-        const response = await axios.get(`${CLIENT_URL}${input}`);
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
-
+export async function getTvShowData(input) {
+  try {
+    //making api call
+    const response = await axios.get(`${CLIENT_URL}${input}`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+export const favoriteLists = [
+  "https://static.tvmaze.com/uploads/images/medium_portrait/28/72341.jpg",
+  "https://static.tvmaze.com/uploads/images/medium_portrait/4/11322.jpg",
+  "https://static.tvmaze.com/uploads/images/medium_portrait/359/898433.jpg",
+  "https://static.tvmaze.com/uploads/images/medium_portrait/402/1006397.jpg",
+  "https://static.tvmaze.com/uploads/images/medium_portrait/362/906711.jpg",
+  "https://static.tvmaze.com/uploads/images/medium_portrait/145/363769.jpg",
+];
