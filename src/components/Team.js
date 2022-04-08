af2004688501c239018d0479a2ca214a52da7c4a
ajitha99 <ajitha.anandapu@gmail.com>
2022-04-07 21:28:34 -0400

    no-data-pic, fixed carousel bug

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,16 +1,21 @@
 function Team (props) {
     // Destructuring
-    const {name, location, email, cell , github, linkedin, introduction} = props.team
+
+    console.log(props)
+    console.log(JSON.stringify(props.teamData.name.first));
+    // const {name, location, email, cell , github, linkedin, introduction} = props.team
+    
+    // console.log(name);
     return (
         <>
         <h1> Team info</h1>
-        <p>name: {name.title} {name.first} {name.last}</p>
-        <p>location: {location}</p>
-        <p>email: {email}</p>
-        <p>cell: {cell}</p>
-        <p> github: {github}</p>
-        <p> linkedin: {linkedin}</p>
-        <p> introduction: {introduction}</p>
+        {/* <p>name: {JSON.stringify(props.teamData.name.title)} {JSON.stringify(props.teamData.name.first)} {JSON.stringify(props.teamData.name.last)}</p> */}
+        <p>location: {props.teamData.location}</p>
+        <p>email: {props.teamData.email}</p>
+        <p>cell: {props.teamData.cell}</p>
+        <p> github: {props.teamData.github}</p>
+        <p> linkedin: {props.teamData.linkedin}</p>
+        <p> introduction: {props.teamData.introduction}</p>
         </>
     )
 }
