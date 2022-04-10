6ee97660fe07d7ce19542397b20af0d31cd74063
bosen L <bosenli2013@hotmail.com>
2022-04-10 16:45:16 -0400

    episode list

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -5,8 +5,14 @@ import { getEpisodeDataById } from "../services/constants";
 function Episodes() {
   const [episodeData, setEpisodeData] = useState([]);
   const { id } = useParams();
+  const [toggle, setToggle] = useState(false)
+
   // console.log(id);
   // console.log(episodeData);
+
+  const handleOnClick = () =>{
+    setToggle(prevState => !prevState)
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,7 +23,40 @@ function Episodes() {
     fetchData();
   }, []);
 
-  return <h1>Hello All</h1>;
+  return (
+    <div>
+    <h1>Episodes</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Number</th>
+          <th>Date</th>
+          <th>Name</th>
+          <th>Rating</th>
+          <th>Summary</th>
+        </tr>
+      </thead>
+      <tbody>
+        {(episodeData||[]).map((episode,index)=>{
+            return (
+              <tr>
+                <td>{episode.number}</td>
+                <td>{episode.airdate}</td>
+                <td>{episode.name}</td>
+                <td>{episode.rating.average}</td>
+                <td><button onClick={handleOnClick}>summary
+                { (toggle)? <div>{episode.summary}</div> : null}
+                </button>
+                
+                </td>
+              </tr>
+            )
+        })}
+     
+    </tbody>
+  </table>
+  </div>
+    );
 }
 
 export default Episodes;
