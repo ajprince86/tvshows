0d5558ef9b058f8885fc15ace51f7693afc5884f
Amari Princivil <ajthacode88@gmail.com>
2022-04-14 17:11:18 -0400

    add more lists

--- src/services/constants.js
+++ src/services/constants.js
@@ -47,6 +47,11 @@ export async function getEpisodeDataById(id) {
 
 export const favoriteLists = [
   {
+    id: 56084,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/399/997839.jpg",
+  },
+
+  {
     id: 7903,
     img: "https://static.tvmaze.com/uploads/images/medium_portrait/28/72341.jpg",
   },
@@ -120,16 +125,116 @@ export const upcomingShows = [
   },
 ];
 
-//Star Trek: Strange New Worlds 48090
-//Obi-Wan Kenobi 52260
-//Resident Evil 46273
-//The Lord of the Rings: The Rings of Power 33352
-//House of the Dragon 44778
-//Gremlins 42842
-//The Sandman 42827
-//Shining Girls 49334
-//The Terminal List 47856
-//Ms. Marvel 43519
+const romance = [
+  {
+    id: 42966,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/398/995479.jpg",
+  },
+  {
+    id: 42848,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/362/905243.jpg",
+  },
+  {
+    id: 42361,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/249/623354.jpg",
+  },
+  {
+    id: 38894,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/257/643559.jpg",
+  },
+];
 
-//If the show has no image we should have the name show up below
-//example is when you search dexter
+const action = [
+  {
+    id: 43031,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/395/988157.jpg",
+  },
+  {
+    id: 44391,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/395/988157.jpg",
+  },
+  {
+    id: 45121,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/395/987549.jpg",
+  },
+  {
+    id: 5079,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/210/525472.jpg",
+  },
+  {
+    id: 28160,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/280/701868.jpg",
+  },
+];
+
+const classic = [
+  {
+    id: 825,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/6/15722.jpg",
+  },
+
+  {
+    id: 437,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/3/9021.jpg",
+  },
+  {
+    id: 722,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/6/15097.jpg",
+  },
+  {
+    id: 665,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/5/14728.jpg",
+  },
+  {
+    id: 1249,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/7/17917.jpg",
+  },
+  {
+    id: 1474,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/8/21115.jpg",
+  },
+];
+
+const anime = [
+  {
+    id: 2103,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/11/29190.jpg",
+  },
+  {
+    id: 555,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/79/199224.jpg",
+  },
+];
+
+const heros = [
+  {
+    id: 43518,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/398/995334.jpg",
+  },
+  {
+    id: 41749,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/295/739854.jpg",
+  },
+  {
+    id: 50603,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/380/950547.jpg",
+  },
+  {
+    id: 41007,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/320/801227.jpg",
+  },
+  {
+    id: 16544,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/179/448895.jpg",
+  },
+  {
+    id: 27557,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/348/871734.jpg",
+  },
+  {
+    id: 15299,
+    img: "https://static.tvmaze.com/uploads/images/medium_portrait/399/998261.jpg",
+  },
+];
+
+//Staff picks
