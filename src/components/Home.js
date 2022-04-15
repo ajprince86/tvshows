d101dfef0c6931c5dcce34795e6d55506ecfd3c7
bosen L <bosenli2013@hotmail.com>
2022-04-14 22:11:47 -0400

    added scolling image

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import Carousel from "react-elastic-carousel";
 import "./home.css";
 import { favoriteLists, upcomingShows } from "../services/constants.js";
 import bgImage from "../videos/website-video.mp4";
-
+import "./home.scss"
 function Home() {
   const navigate = useNavigate();
   const breakPoints = [
@@ -51,7 +51,7 @@ function Home() {
     </div>
 
       <div>
-      <h2 className="home-color">Upcoming Shows</h2>
+       <h2 className="home-color">Upcoming Shows</h2>
         <Carousel breakPoints={breakPoints}>
           {upcomingShows.map((show, key) => {
             return (
@@ -66,6 +66,18 @@ function Home() {
             );
           })}
         </Carousel>
+      </div>
+      <div className = "scolling-img">
+        <img src='https://static.tvmaze.com/uploads/images/medium_portrait/28/72341.jpg' alt=''/>
+      <img src='https://static.tvmaze.com/uploads/images/medium_portrait/399/997839.jpg' alt=''/>
+<img src='https://static.tvmaze.com/uploads/images/medium_portrait/4/11322.jpg' alt=''/>
+<img src='https://static.tvmaze.com/uploads/images/medium_portrait/359/898433.jpg' alt=''/>
+<img src='https://static.tvmaze.com/uploads/images/medium_portrait/362/906711.jpg' alt=''/>
+<img src='https://static.tvmaze.com/uploads/images/medium_portrait/145/363769.jpg' alt=''/>
+<img src='https://static.tvmaze.com/uploads/images/medium_portrait/394/986248.jpg' alt=''/>
+<img src='https://static.tvmaze.com/uploads/images/medium_portrait/400/1001018.jpg' alt=''/>
+<img src='https://static.tvmaze.com/uploads/images/medium_portrait/394/986215.jpg' alt=''/>
+<img src='https://static.tvmaze.com/uploads/images/medium_portrait/396/991595.jpg' alt=''/>
       </div>
     </div>
   );
